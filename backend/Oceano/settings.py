1c6c2801196842cdba90445cab0a13ae4b570673
Omar Virili <omar.virili@gmail.com>
2024-07-31 21:56:12 -0300

    Update settings.py
    dejo CSFR_COOKIE_HTTONLY en FALSE

--- backend/Oceano/settings.py
+++ backend/Oceano/settings.py
@@ -147,13 +147,9 @@ REST_FRAMEWORK = {
     ]
 }
 #Solo envía la cookie sobre HTTPS
-#cambio la linea a false para probar Omar
-CSRF_COOKIE_SECURE = False
-#Previene acceso por JavaScript tambien lo cambio a false Omar
+CSRF_COOKIE_SECURE = True
+#Previene acceso por JavaScript solo dejo esta en false 
 CSRF_COOKIE_HTTPONLY = False
-#Agrego esta linea SESSION_COOKIE_HTTPONLY = False
-SESSION_COOKIE_SECURE = False
-SESSION_COOKIE_HTTPONLY = False
 #1 semana, en segundos
 SESSION_COOKIE_AGE = 604800
 
