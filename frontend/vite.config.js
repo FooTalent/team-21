886419fa270b0fab4786cc67205ec73dc6028576
odv144 <omar.virili@gmail.com>
2024-08-03 17:08:51 -0300

    Cambio del mismo commit 837eadf

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig(({ mode }) => {
       }
     },
     plugins: [react()],
-    base: mode === 'production' ?  '/laputita/' : '/',
+    base: mode === 'production' ?  'https://hotel-oceano.onrender.com' : '/',
     build: {
       outDir: 'dist',
     },
