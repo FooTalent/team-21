641bbbaa630fff5a9fe2bbbeefdc3e99732112c0
odv144 <omar.virili@gmail.com>
2024-08-06 07:52:04 -0300

    configurando para produccion

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,34 +5,26 @@ export default defineConfig(({ mode }) => {
   // Cargar las variables de entorno
   const env = loadEnv(mode, process.cwd());
   const apiURL = env.VITE_API_URL;
-  if (mode !== "production") {
-    return {
-      server: {
-        proxy: {
-          "/api": {
-            target: apiURL,
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/api/, ""),
-            secure: false,
-            // Añade este campo para permitir el manejo de cookies
-            cookieDomainRewrite: "localhost",
-          },
+
+  return {
+    server: {
+      proxy: {
+        "/api": {
+         
+           target: apiURL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+          secure: false,
+          // Añade este campo para permitir el manejo de cookies
+          cookieDomainRewrite: "localhost",
         },
       },
-      plugins: [react()],
-      // base: mode === 'production' ? apiURL : '/',
-      base: "/",
-      build: {
-        outDir: "dist",
-      },
-    };
-  } else {
-    return {
-      base: "/",
-      build: {
-        outDir: "dist",
-      },
-      plugins: [react()],
-    };
-  }
+    },
+    plugins: [react()],
+    // base: mode === 'production' ? apiURL : '/',
+    //base: '/',
+    build: {
+      outDir: "dist",
+    },
+  };
 });
